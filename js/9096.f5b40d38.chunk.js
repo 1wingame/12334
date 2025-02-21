a64545bab35ad90b484c62dbf07a9eb8cfbf2409
1wingame <qqqw1448@gmail.com>
2025-02-21 19:32:22 +0500

    Add files via upload

---
+++ js/9096.f5b40d38.chunk.js
@@ -0,0 +1,6 @@
+"use strict";
+(self.webpackChunkcoin_flip_app_ts = self.webpackChunkcoin_flip_app_ts || []).push([[9096], {
+    9096: function(e) {
+        e.exports = JSON.parse('{"Traps":"traps","PopupStep1":"Mines is a gambling game based on the classic \u201cMinesweeper\u201c. Your goal is to open safe cells and not to fall into traps.","PopupStep2":"Enter the bet amount and select the number of traps on the field. Click the \u201cPlay\u201c button to place a bet and start playing.","PopupStep3":"Each cell contains either a star or a cross-trap. Each open star increases the winning coefficient, and the cross-trap finishes the game.","PopupStep4":"You can always check the fairness of the outcomes generated. All data of the completed games is stored in the betting history and provable. Check the hash in any online generator."}')
+    }
+}]);
